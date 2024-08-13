7c95642a7f1eeba6109caf4b5818fa134b105607
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-08-13 22:30:19 +0530

    add

--- 01_basics/MCQ.java
+++ 01_basics/MCQ.java
@@ -3,6 +3,6 @@ public class MCQ {
         int var1 = 5;
         int var2 = 6 ;
         if((var2 = 1) == var1)System.out.print(var2);
-        else System.out.print(++var2);
+        else System.out.print(++var2);//
    }
 }
