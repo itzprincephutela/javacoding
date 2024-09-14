ad5d4d72a172ad38a7077e420b432f39634adf1e
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-09-14 23:07:27 +0530

    ADD

---
+++ OOPS/PWOOPS/PracticeQus/8Assigment/Qus1.java
@@ -0,0 +1,22 @@
+class Account {
+    private int  AccountNumber ,Balance ;
+    private String CustomreName ;
+    private static int rateOfintrest;
+    
+    public void SetAccountNumber(int AN){
+        AccountNumber = AN;
+    }
+    public void SetBalance(int B){
+        Balance = B;
+    }
+    public void SetCustomreName(String N){
+        CustomreName = N;
+    }
+    public static void rateOfintrest(int I){
+        rateOfintrest = I;
+    }
+    
+}
+public class Qus1 {
+    
+}
---
+++ OOPS/PWOOPS/PracticeQus/8Assigment/Qus2.java
@@ -0,0 +1,16 @@
+// class SuperClass{
+//     private String ownerName ;
+//     private double price;
+//     private String colour;
+//     private String brand ;
+//     private SuperClass(){
+//         //is se bhar koi bi object nahi bn shkta 
+//     }
+//     private static int countInstance;
+//     public static SuperClass getInstance(){
+
+//     }
+// }
+// public class Qus2 {
+    
+// }
