96d72ea51491854b10bafa59bc1f1e2f6dde877f
Prince kumar <122357150+itzprincephutela@users.noreply.github.com>
2024-10-15 23:38:16 +0530

    commit

---
+++ New folder/LinkedList/basic.java
@@ -0,0 +1,14 @@
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+    }
+}
+public class basic {
+    public static void main(String[] args) {
+       
+   
+
+      }
+}
